192dc35a6ea60333da204d3764ce13ce5afe33a0
3akare <bakaredavid007@gmail.com>
2024-09-02 19:51:28 +0100

    style: prettier code style

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@
 function App() {
   return (
     <>
-      <div className="text-red-900 text-xl font-semibold">
-        PayCraft
-      </div>
+      <div className="text-red-900 text-xl font-semibold">PayCraft</div>
     </>
   );
 }
