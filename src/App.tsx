87a8688bb7e3febdea854e2e36b401f2239e782a
3akare <bakaredavid007@gmail.com>
2024-09-02 09:03:39 +0100

    build: add react query dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import DisplayPosts from "./components/DisplayPosts";
 import { Button } from "./components/ui/button";
 
 // Desc: Main App component for the application.
@@ -6,6 +7,8 @@ function App() {
     <>
       <div className="text-red-900 text-xl font-semibold">
         <Button variant="destructive">paycraft-frontend</Button>
+         <br />
+        <DisplayPosts/>
       </div>
     </>
   );
