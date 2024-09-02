dffe5c39143f64863d51619ca66761bee2d7f272
3akare <bakaredavid007@gmail.com>
2024-09-02 19:50:39 +0100

    refactor: complete build configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import DisplayPosts from "./components/DisplayPosts";
-import { Button } from "./components/ui/button";
-
 // Desc: Main App component for the application.
 function App() {
   return (
     <>
       <div className="text-red-900 text-xl font-semibold">
-        <Button variant="destructive">paycraft-frontend</Button>
-         <br />
-        <DisplayPosts/>
+        PayCraft
       </div>
     </>
   );
