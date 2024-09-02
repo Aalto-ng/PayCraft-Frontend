134b7a394fab1d8f6ddde64eb7b6e90e2bc52ca6
3akare <bakaredavid007@gmail.com>
2024-09-02 08:54:22 +0100

    build: add dependecies and build scripts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+// Desc: Main App component for the application.
+function App() {
+  return (
+    <>
+      <div className="text-red-900 text-xl font-semibold">
+        <Button variant="destructive">paycraft-frontend</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
