58cd5c518fff1353cc754f0be43a2281f7972abe
3akare <bakaredavid007@gmail.com>
2024-09-05 16:02:40 +0100

    Login handler

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { AuthProvider } from "./context/AuthProvider.tsx";
 
 import App from "./App.tsx";
 import "./index.css";
+import { Toaster } from "./components/ui/toaster.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </StrictMode>
-  </QueryClientProvider>
+  <StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <App />
+          <Toaster />
+        </Router>
+      </QueryClientProvider>
+    </AuthProvider>
+  </StrictMode>
 );
